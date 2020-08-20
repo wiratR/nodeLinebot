21b24515e0f98ef5120b2a376b2623f0b1ce7446
wiratR <wirateds@gmail.com>
2020-08-20 14:27:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
-    let reply_token = req.body.events[0].replyToken
-    reply(reply_token)
+    //let reply_token = req.body.events[0].replyToken
+    //reply(reply_token)
     res.sendStatus(200)
 })
+/*
 app.listen(port)
 function reply(reply_token) {
     let headers = {
@@ -36,4 +37,4 @@ function reply(reply_token) {
     }, (err, res, body) => {
         console.log('status = ' + res.statusCode);
     });
-}
+}*/
